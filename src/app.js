f036e495a97202540bb628cdbd0efb94409e952b
wonhee.jung <wonhee.jung@nextfloor.co.kr>
2019-06-07 17:25:22 +0900

    미세먼지 api 추가

--- src/app.js
+++ src/app.js
@@ -5,16 +5,19 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const reply = require('./reply');
 // Add routes
+const actionBasic = require('./action/basic');
+const actionError = require('./action/error');
+const actionHelp = require('./action/help');
+const actionEnjoy = require('./action/enjoy');
+const actionScheduler = require('./action/scheduler');
 // for LINE API
-// TODO: Hide for security
-const CHANNEL_ACCESS_TOKEN = "";
+const CHANNEL_ACCESS_TOKEN = "NxcrY/NSnfmMUwHUBnk92OUkaBrlBfF128P7Bva4E4O5AsUD4gl9XSNCdxAY1mqfyP3WFvbutIwFYpxPTHdNaakt3RBqL0KjN6Ex4bUVqZpw1M1xWVdWLHIUfInQCf/rElQmam9lLeTAQ5jsZw+q4AdB04t89/1O/w1cDnyilFU=";
 // for SSL
-// TODO: Hide for security
-// var https_options = {
-//     ca: fs.readFileSync('utf8'),
-//     key: fs.readFileSync('utf8'),
-//     cert: fs.readFileSync('utf8')
-//  };
+var https_options = {
+    ca: fs.readFileSync('/etc/letsencrypt/live/weather-bot.online/chain.pem', 'utf8'),
+    key: fs.readFileSync('/etc/letsencrypt/live/weather-bot.online/privkey.pem', 'utf8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/weather-bot.online/fullchain.pem', 'utf8')
+ };
 // set Express
 var app = express();
 app.use(bodyParser.json());
@@ -28,13 +31,13 @@ app.post('/hook', function (request, response) {
     var eventObj = request.body.events[0];
     var message = eventObj.message;
     var request = require('request');
+    // 전역 변수 사용
     var data = [{
         "type": "text",
         "text": "data",
     }]
-    if (message.type === "text" && message.text === "날씨") {
-        //TODO: HIde for security
-        request.get('', function(err, res, body) {
+    if (message.type === "text" && (message.text === "날씨" || message.text.includes("날씨"))) {
+        request.get('http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=47606880441212d2f8a5f073e0b044e4', function(err, res, body) {
             weatherData = JSON.parse(body);
             data[0].text = "현재 서울의 온도: "
                             +(weatherData.main.temp-273.15).toFixed(2)+"℃,"
@@ -42,6 +45,26 @@ app.post('/hook', function (request, response) {
                            " 구름: "+weatherData.clouds.all+"%,"+
                            " 바람: "+weatherData.wind.speed+"ms,"+
                            " 전반적으로 "+weatherData.weather[0].main+"합니다.\ "
+            reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, data);
+        });
+    } else if (message.type === "text" && (message.text === "미세먼지" || message.text.includes("미세먼지"))) {
+        //TODO: HIde for security // 37.511168,127.029248,15z
+        request.get('http://api.airvisual.com/v2/nearest_city?lat=37.511168&lon=127.029248&rad=500&key=GEfFaZztZy9bMJr6m', function(err, res, body) {
+            dustData = JSON.parse(body);
+            var aqius= dustData.data.current.pollution
+            var result
+            if (aqius < 30) {
+                result = "좋음";
+            } else if (30 <= aqius < 50) {
+                result = "보통";
+            } else if (50 <= aqius < 70) {
+                result = "나쁨";
+            } else {
+                result = "최악"        
+            } 
+
+            data[0].text = "현재 서울의 미세먼지: " +result + "입니다."
+                            
             reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, data);
         });
     } else {
